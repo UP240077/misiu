9d324654304f38ff0919f35c2425ae52cbec0bf7
Angel Correa <correo@ejemplo.com>
2026-07-27 16:43:31 -0600

    segundo commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserDto} from './dto/user.dto'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('registro')
+  registro(@Body()userDto:UserDto): boolean{
+    return true
   }
 }
